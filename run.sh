9700a3ba065576565b80de3c8211f684a6270197
outsidethecode <49871473+outsidethecode@users.noreply.github.com>
2022-01-23 05:10:36 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo "Starting run.sh"
 ifconfig lo 127.0.0.1
 
 # Add a hosts record, pointing API endpoint to local loopback
-echo "127.0.0.1   kms.eu-central-1.amazonaws.com" >> /etc/hosts
+echo "127.0.0.1   kms.us-east-2.amazonaws.com" >> /etc/hosts
 
 # Run traffic forwarder in background and start the server
 nohup python3 /app/traffic-forwarder.py 443 3 8000 &
